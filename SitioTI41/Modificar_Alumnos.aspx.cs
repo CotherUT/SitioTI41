28edee45ca015271a8e0820e7bcec9fe62bed4cb
CotherUT <tic-290025@utnay.edu.mx>
2022-11-16 08:32:56 -0700

    lo del faceboo

--- SitioTI41/Modificar_Alumnos.aspx.cs
+++ SitioTI41/Modificar_Alumnos.aspx.cs
@@ -9,7 +9,7 @@ namespace SitioTI41
 {
     public partial class Modificar_Alumnos : System.Web.UI.Page
     {
-        int cotIdAlumno; //variable declarada de forma global para que sea vista portodos los eventos
+        public static int cotIdAlumno; //variable declarada de forma global para que sea vista portodos los eventos
         protected void Page_Load(object sender, EventArgs e)
         {
             tbDatos.Visible = false;
@@ -52,8 +52,22 @@ namespace SitioTI41
         protected void btnGuardar_Click(object sender, ImageClickEventArgs e)
         {
             string cotNombres, cotPaterno, cotMaterno, cotEmail;
-            char sexo;
+            char cotSexo;
             int cotIdCarrera, cotIdGrupo;
+
+            cotNombres = tbNombres.Text;
+            cotPaterno = tbPaterno.Text;
+            cotMaterno = tbPaterno.Text;
+            cotEmail = tbEmail.Text;
+            cotSexo = char.Parse(rblSexo.Text);
+            cotIdGrupo = int.Parse(ddlGrupo.Text);
+            cotIdCarrera = int.Parse(ddlCarrera.Text);
+
+            utTableAdapters.AlumnosTableAdapter taAlumnos = new utTableAdapters.AlumnosTableAdapter();
+            taAlumnos.Update(cotNombres, cotPaterno,cotMaterno,cotSexo.ToString(),cotEmail,cotIdGrupo,cotIdCarrera,cotIdAlumno);
+
+            string script = "javascript:alert('Campo ID vacio, ingresa un ID correcto');";
+            ScriptManager.RegisterStartupScript(this, GetType(), "alerta", script, true);
         }
     }
 }
