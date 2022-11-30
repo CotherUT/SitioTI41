ca0fa6579214fad3604948ee4c62527d72b49563
CotherUT <tic-290025@utnay.edu.mx>
2022-11-30 07:12:51 -0700

    file upload

---
+++ SitioTI41/subir_archivo.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SitioTI41
+{
+    public partial class subir_archivo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubirArchivos_Click(object sender, EventArgs e)
+        {
+            //Si el usuario le dio al boton de examinar
+            if (archivos.HasFile)
+            {
+                try
+                {
+                    //El nombre que tiene el archivo
+                    string nombre_archivo = Path.GetFileName(archivos.FileName);
+                    //La ruta donde se va a guardar el archivo
+                    archivos.SaveAs("C:\\Users\\LAB10-PC10\\Documents\\archivos_sitioti41\\" + archivos.FileName);
+                    //redireccionar al usuario cunado el archivo se ha subido conexito
+                    Response.Redirect("Subidos_Exitosos.html");
+                }
+                catch (Exception ex)
+                {
+                    lblError.Text = "Estado de carga: No se puede subir el archivo, ocurrio el siguiente error: " + ex.Message;
+                }
+            }
+        }
+    }
+}
---
+++ SitioTI41/subir_archivo.aspx.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace SitioTI41
+{
+
+
+    public partial class subir_archivo
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control archivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload archivos;
+
+        /// <summary>
+        /// Control btnSubirArchivos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubirArchivos;
+
+        /// <summary>
+        /// Control lblError.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblError;
+    }
+}
